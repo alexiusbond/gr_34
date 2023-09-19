47439f0fa0b131b7e376216d34b7da412e139efe
alexiusbond <aleksey.bondarev@iaau.edu.kg>
2023-09-19 19:46:05 +0600

    Added comment

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ from termcolor import cprint
 import emoji
 
 print('Hello from CMD!')
-
+# Hi Sensei!
 cprint("Hello, World!", "green", "on_red", attrs=["bold", "underline"])
 print(emoji.emojize('Python is :thumbs_up:'))
